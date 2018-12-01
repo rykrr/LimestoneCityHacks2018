ecd77099ea5856f9d6bfdeb769d05ce04af1756f
rykrr <hiroshikerr@gmail.com>
2018-12-01 18:36:46 -0500

    api

--- twi.py
+++ twi.py
@@ -13,9 +13,12 @@ def authorize():
     headers = {'Authorization': 'Basic {}'.format(credentials)}
     data = {'grant_type':'client_credentials'}
     r = requests.post('https://api.twitter.com/oauth2/token', headers=headers, data=data)
-
+    
     if r.status_code == 200:
+        print('Token Generated')
         return r.json()['access_token']
+    print(r.status_code)
+    print(r.text)
     return None
 
 def invalidate(token):
@@ -27,5 +30,11 @@ def invalidate(token):
     r = requests.post('https://api.twitter.com/oauth2/invalidate_token', headers=headers, data=data)
     print(r.status_code)
 
-token = authorize();
-invalidate(token);
+def search(token, query_string):
+    headers = {'Authorization': 'Bearer {}'.format(token)}
+    query = urllib.parse.quote_plus(query_string)
+    r = requests.get('https://api.twitter.com/1.1/search/tweets.json?l=en&q={}&count=100'.format(query), headers=headers)
+    
+    if r.status_code == 200:
+        return r.text
+    return '[]'
