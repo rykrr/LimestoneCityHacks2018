7b5e022622ad28a49a2e99a420fa578a41a71ef2
Ryan Kerr <hiroshikerr@gmail.com>
2018-12-01 15:07:05 -0500

    auth

--- api.py
+++ api.py
@@ -1,11 +1,6 @@
 from flask import Flask
-from flask import request
-from flask import send_file
-
-import requests
 
 app = Flask(__name__)
-key = ''
 
 @app.route('/callback', methods=['POST','GET'])
 def _callback():
@@ -14,11 +9,6 @@ def _callback():
 @app.route('/mlh/emomap/tag/<tag>', methods=['POST'])
 def _tag(tag):
     return tag
-
-def query(tag):
-    url = 'https://api.twitter.com/1.1/tweets/search/7day/Prod.json'
-    headers = {'authorization':'Bearer 1068901758318067712-rWTsS7K8CFeMTgT7JT4IQe9UbA2zic'}
-    data = {'query':'from:JimWatsonOttawa lang:en', 'maxResults':16, 'fromDate':
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6969)
