bf5c5c6b069936b4afb5f812364114a27806012c
Kenya Moore <kenyacmoore@gmail.com>
2018-12-01 20:54:20 -0500

    p
    p

--- java.js
+++ java.js
@@ -175,7 +175,7 @@ function initMap() {
 ],
 	  center: regina
 	  });
-  var cityCircle = new google.maps.Circle({
+  /*var cityCircle = new google.maps.Circle({
             strokeColor: '#FF0000',
             strokeOpacity: 0.8,
             strokeWeight: 2,
@@ -185,14 +185,223 @@ function initMap() {
             center: {lat: 44.2312, lng: -76.4860},
             radius: 100000
           });
+		 */
+		 
 } 
+
+function colors(tweetArr, i){
+	console.log(tweetArr[i].tones);
+	var color;
+	if(tweetArr[i].tones == []){
+		color = "black";
+	}
+	else{
+		color = "red";
+	}
+	return color;
+}
   
 var jsonTweet;
 function searched() {
-    var search = document.getElementById("searchField").value;
+    
+	
+  var regina = {lat: 50.4452, lng: -104.6189};
+  
+  var map = new google.maps.Map(
+      document.getElementById('map'), {
+	  zoom: 4, 
+	  styles : [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+	
+],
+	  center: regina
+	  });
+	  var search = document.getElementById("searchField").value;
     $.get('http://10.217.143.21:6969/test/query/' + search, function(result) {
-        console.log(JSON.parse(result))
-    })
+        jsonTweet = JSON.parse(result);
+		var tweetLen = jsonTweet.length;
+		console.log(jsonTweet);
+			  for(var i = 0; i < 10; i++){
+				  console.log(i);
+				  var cityCircle = new google.maps.Circle({
+							strokeColor: colors(jsonTweet, i),
+							strokeOpacity: 0.8,
+							strokeWeight: 2,
+							fillColor: colors(jsonTweet, i),
+							fillOpacity: 0.35,
+							map: map,
+							center: coords(),
+							radius: 100000
+						  });
+			  }
+	  
+    });
+		 
+	
 }
  
  function setTweetTexts(){
@@ -204,9 +413,9 @@ function searched() {
  } 
  
  function coords(){
-	 var lat = (Math.random() * (68.3607 - 31.7619) + 31.7619).toFixed(4)*1;
+	 var lati = (Math.random() * (68.3607 - 31.7619) + 31.7619).toFixed(4)*1;
 	 var longi = (Math.random() * (130.3208 - 71.2080) + 71.2080).toFixed(4)*-1;
-	 return {lat, longi};
+	 return {lat: lati, lng: longi};
 	 
  }
  
